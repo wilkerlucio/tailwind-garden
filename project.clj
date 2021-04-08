8c378b7306aa2212567909d28338380f96597fc6
Wilker Lucio <wilkerlucio@gmail.com>
2021-04-08 13:44:39 -0300

    Bump and fix dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.wsscode/tailwind-garden "2021.04.08"
+(defproject com.wsscode/tailwind-garden "2021.04.08-1"
   :description ""
-  :url "https://github.com/wilkerlucio/"
+  :url "https://github.com/wilkerlucio/tailwind-garden"
   :min-lein-version "2.7.0"
   :license {:name "Eclipse Public License - v 2.0"
             :url  "https://opensource.org/licenses/EPL-2.0"}
-  :dependencies []
+  :dependencies [[garden/garden "1.3.10"]]
 
   :source-paths ["src/main" "resources"]
 
